3329531882f7aeaaabdeda8f71db630c7b32867d
Kevin <Kevin@KEVIN>
2022-04-08 20:30:01 -0600

    Tarea #8 | Agregando usuario, encriptado y autoirzacion de controladores

---
+++ KidsToys/KidsToys.BL/DatosdeInicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsToys.BL
+{
+    public class DatosdeInicio: CreateDatabaseIfNotExists<Contexto>
+    {
+        protected override void Seed(Contexto contexto)
+        {
+            var nuevoUsuario = new Usuario();
+
+            nuevoUsuario.Nombre = "admin";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("123");
+
+            nuevoUsuario.Nombre = "Abigail";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("456");
+
+            nuevoUsuario.Nombre = "Daniel";
+            nuevoUsuario.Contrasena = Encriptar.CodificarContrasena("789");
+
+            contexto.Usuarios.Add(nuevoUsuario);
+
+            base.Seed(contexto);
+        }
+    }
+}
